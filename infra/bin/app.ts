5e89fb9dd7148ac6040edf5d0f13ec3fbe110b3c
lequockhanh19521680 <19521680@gm.uit.edu.vn>
2026-01-20 12:19:36 +0700

    fix some bug fe

--- infra/bin/app.ts
+++ infra/bin/app.ts
@@ -1,11 +1,24 @@
 #!/usr/bin/env node
+import "dotenv/config";
 import * as cdk from "aws-cdk-lib";
 import { ProcessingFileISOPipingStack } from "../lib/stack";
 
 const app = new cdk.App();
-new ProcessingFileISOPipingStack(app, "ProcessingFileISOPipingStack", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
+
+// Ưu tiên lấy từ biến môi trường, hoặc hard-code luôn 'ap-southeast-1' nếu muốn chắc chắn
+const env = {
+  account:
+    process.env.CDK_DEPLOY_ACCOUNT ||
+    process.env.CDK_DEFAULT_ACCOUNT ||
+    "492098925520",
+  region:
+    process.env.CDK_DEPLOY_REGION ||
+    process.env.CDK_DEFAULT_REGION ||
+    "ap-southeast-1",
+};
+
+new ProcessingFileISOPipingStack(app, "ProcessingFileISOPipingStack", { env });
+
+console.log(
+  `Preparing deploy for Account: ${env.account} | Region: ${env.region}`,
+);
